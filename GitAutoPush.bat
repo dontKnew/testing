549611b6143d018e128e2d1b31ae538b13be6473
dontknew <israfil123.sa@gmail.com>
2022-11-01 21:01:34 +0530

    auto pushed

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -1,12 +1,8 @@
-:loop
+::loop
 	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\sajid\Desktop\automatic_push_code_to_github\test2
 	cd C:\Users\sajid\Desktop\automatic_push_code_to_github\test2
-	
-	echo "push starting..."
-	::Initialize GitHub
-	git init
 	
 	::Pull any external changes (maybe you deleted a file from your repo?)
 	git pull origin master
@@ -17,19 +13,17 @@
 	::Commit all changes with the message "auto push". 
 	::Change as needed.
 	git commit -m "auto pushed"
-	
-	::Add origin 
-	git remote add origin https://github.com/dontKnew/testing.git
 
 	::Push all changes to GitHub 
 	git push origin master
 	
 	::Alert user to script completion and relaunch.
-	echo Complete. Relaunching...
+	echo Completed. Relaunching...
 	
 	::Wait 300 seconds until going to the start of the loop.
 	::Change as needed.
-	TIMEOUT 300
+	TIMEOUT 50
+	EXIT
 	
 ::Restart from the top.	
-goto loop
+::goto loop
